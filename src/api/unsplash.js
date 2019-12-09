cef20755a221d1b1b84d13f2303c46206eb8f009
Nino3103 <jeangilardo@gmail.com>
2019-12-09 15:25:00 +0100

    security changes

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers:{
-        Authorization: 'Client-ID 1acd11f0671ea428527af7d7df243ce6ad3f527a0be886d6e63591c189b1d025'
+        Authorization: 'Client-ID YOUR_ID'
     }
 
 })
